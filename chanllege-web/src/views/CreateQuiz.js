7db55c13257ae2970297bbab83de6ce05b6e8f4c
jossias <mupandzaj@gmail.com>
2021-03-03 23:13:10 +0200

    Adds form to create Quiz

--- chanllege-web/src/views/CreateQuiz.js
+++ chanllege-web/src/views/CreateQuiz.js
@@ -1,9 +1,46 @@
 import React from "react";
 import MainLayout from "../layouts/MainLayout";
-import {Button, Card, Col, Container, Row} from "reactstrap";
+import {Button, Card, Col, Container, Row, Label} from "reactstrap";
 import { AvForm, AvField, AvRadioGroup, AvRadio } from "availity-reactstrap-validation";
+import swal from '@sweetalert/with-react'
 
 export default function CreateQuiz() {
+
+    const handleSubmit = (e, data) => {
+        e.preventDefault();
+        console.log(data);
+
+        swal({
+            title: "Atenção!",
+            icon: "warning",
+            text: "Copie e guarde os códigos/links do seu inquérito num lugar seguro. Caso perca os" +
+                " códigos/links não terá como aceder ou editar seu inquérito. ",
+            content: (
+                <div>
+                    <Row>
+                        <p>
+                            <Label className="text-black-50">Código de Edição (privado, usado para editar questões, visualizar todas respostas)</Label>
+                            <span>edit-kjnjvjbjsdkbvjdbvdfkvbkfbjv</span>
+                        </p>
+                        <p>
+                            <Label>Link de Edição (privado, usado para editar questões, visualizar todas respostas)</Label>
+                            <span>http://pesquisapp.com/edit-kjnjvjbjsdkbvjdbvdfkvbkfbjv</span>
+                        </p>
+                    </Row>
+                    <Row>
+                        <p>
+                            <Label>Codigo de visualização (Público, usado por seus inquiridos para enviar respostas)</Label>
+                            <span>quiz-kjnjvjbjsdkbvjdbvdfkvbkfbjv</span>
+                        </p>
+                        <p>
+                            <Label>Link de visualização (Público, usado por seus inquiridos para enviar respostas)</Label>
+                            <span>http://pesquisapp.com/quiz-kjnjvjbjsdkbvjdbvdfkvbkfbjv</span>
+                        </p>
+                    </Row>
+                </div>
+            )
+        })
+    }
 
     return(
         <MainLayout>
@@ -43,7 +80,7 @@ export default function CreateQuiz() {
                         </Row>
                         <Row>
                             <Col sm={12}>
-                                <AvForm>
+                                <AvForm onValidSubmit={handleSubmit}>
                                     <Row>
                                         <Col md={6}>
                                             <AvField
@@ -93,7 +130,7 @@ export default function CreateQuiz() {
                                         </AvRadioGroup>
                                     </Row>
                                     <Row className="justify-content-end">
-                                        <Button color="primary" type="button">
+                                        <Button color="primary" type="submit">
                                             Criar
                                         </Button>
                                     </Row>
